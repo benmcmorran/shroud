1dae1933f6373210c206e9d8d966e382fe8523b2
Ben McMorran <ben.mcmorran@gmail.com>
2021-07-10 01:07:03 -0700

    Add install and artifact upload

--- test/test.ps1
+++ test/test.ps1
@@ -1,4 +1,5 @@
-$output = & "$PSScriptRoot\..\build\release\src\tracebld.exe" /m:mapping.json cmd.exe /c "type wrong.txt"
+$output = & "$PSScriptRoot\..\out\install\release\bin\tracebld.exe" /m:mapping.json cmd.exe /c "type wrong.txt"
+$output
 if (($output -join " ").Contains("correct output")) {
     exit 0
 } else {
